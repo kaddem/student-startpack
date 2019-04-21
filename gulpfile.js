ef2975e80a7afc65323791504a3ecc23fee610ee
Kirill Demidov <kademidov@gmail.com>
2019-04-22 02:00:33 +0300

    добавлена минификация изображений и стилей

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,25 @@
 'use strict';
 
-const { series, parallel, src, dest, watch } = require('gulp');
+const gulp = require('gulp');
 const less = require('gulp-less');
 const postcss = require('gulp-postcss');
 const mqpacker = require('css-mqpacker');
 const sourcemaps = require('gulp-sourcemaps');
 const notify = require('gulp-notify');
+const cssnano = require('gulp-cssnano');
+const responsive = require('gulp-responsive');
 const browserSync = require('browser-sync').create();
 
 // Path
 const path = {
     www: {
         style: 'www/style/',
-        html : 'www/*.html'
+        html : 'www/*.html',
+        images: 'www/images/'
     },
     src: {
-        style: 'src/styles/*.less'
+        style: 'src/styles/*.less',
+        images: 'src/images/**/*.{jpg,png,jpeg,webp}'
     },
     watch: {
         srcStyle   : 'src/styles/**/*.less',
@@ -26,7 +30,7 @@ const path = {
 
 // Compilation less
 function styles() {
-    return src(path.src.style)
+    return gulp.src(path.src.style)
         .pipe(sourcemaps.init())
         .pipe(less()
             .on('error', notify.onError({
@@ -41,27 +45,43 @@ function styles() {
                 sort: true
             })
         ]))
+        .pipe(cssnano())
         .pipe(sourcemaps.write())
-        .pipe(dest(path.www.style));
+        .pipe(gulp.dest(path.www.style));
 }
 exports.styles = styles;
+
+function images() {
+    return gulp.src(path.src.images)
+        .pipe(responsive({
+            '**/*': {
+                width: '100%'
+            }
+        }, {
+            quality: 80,
+            progressive: true,
+            withMetadata: false,
+        }))
+        .pipe(gulp.dest(path.www.images))
+}
+exports.images = images;
 
 function serve() {
     browserSync.init({
         server: "./www"
     });
 
-    watch([
+    gulp.watch([
         path.watch.srcStyle
     ], styles);
 
-    watch([
+    gulp.watch([
         path.watch.html,
         path.watch.buildStyle
     ]).on('change', browserSync.reload);
 }
 
-exports.default = series(
-  parallel(styles),
+exports.default = gulp.series(
+  gulp.parallel(styles, images),
   serve
 );
